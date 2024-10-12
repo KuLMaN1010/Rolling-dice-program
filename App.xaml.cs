2d49a188bd54b227d60899905ee1fe60452d74ef
KuLMaN1010 <sanjidaislam5j5@gmail.com>
2024-10-12 12:41:46 -0500

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Exam_question
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
+    }
+}
---
+++ sanjida_islam_roll_dice_question.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.Maui.Controls;
+
+namespace Exam_question
+{
+    public partial class MainPage : ContentPage
+    {
+        Random random = new Random();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRollDiceClicked(object sender, EventArgs e)
+        {
+            int roll = random.Next(1, 7);
+            DiceLabel.Text = GenerateDiceText(roll);
+        }
+
+        private string GenerateDiceText(int roll)
+        {
+            switch (roll)
+            {
+                case 1:
+                    return "*";
+                case 2:
+                    return "* *";
+                case 3:
+                    return " *\n  *\n   *";
+                case 4:
+                    return "* \n *";
+                case 5:
+                    return "* *\n  *\n * *";
+                case 6:
+                    return "* * \n * *\n * *";
+                default:
+                    return "";
+            }
+        }
+    }
+}
